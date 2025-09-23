3cba62eecc52a2e8e7e44befe43545bf20ce3db4
mstripathi01 <mayank.tripathi218@gmail.com>
2025-09-23 22:43:59 +0530

    changes server.js and dotenv

--- server/server.js
+++ server/server.js
@@ -6,13 +6,24 @@ dotenv.config();
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+console.log("ocess.env.MONGO_URI", process.env.MONGO_URI);
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ status: "ok", server: process.env.SERVER_URL });
+});
 
 // app.use('/api', require('./routes/auth'));
 
@@ -21,7 +32,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log("Server running on port " + process.env.PORT);
+      console.log("Server running on port " + process.env.SERVER_URL);
     });
   })
   .catch((err) => console.error("MongoDB connection failed:", err));
