73f030b7ae0934f7bac3d918501660b926965f9a
mstripathi01 <mayank.tripathi218@gmail.com>
2025-09-24 20:02:42 +0530

    changes in env

--- server/server.js
+++ server/server.js
@@ -9,12 +9,25 @@ const app = express();
 
 // Middleware
 // app.use(cors());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
+// Define the allowed origin
+const allowedOrigins = ["https://myapp-client-xgow.onrender.com"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg =
+        "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+};
+
+// Use the cors middleware with your options
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // Routes
